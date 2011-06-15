37ae30f6916713a13c2534d6ad53abf4d66753eb
salsa <salsa@nan.(none)>
2011-06-15 15:07:57 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LevelUp::Application.config.secret_token = '59b037efca34528c93382fde7dd1bc0c90abd2a85d85b731d8dd2f1aca0ec30de5b15001e82bee70e3f6b4b6815c4a16b93b5c18e5b0d1ce3a894c345e1388d1'
